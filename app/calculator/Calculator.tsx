a577db59016796600e142e7b1d01259d0637bc4f
HuyN2105 <huyn.dev.2105@gmail.com>
2024-07-11 19:54:44 -0700

    progress w ui

--- app/calculator/Calculator.tsx
+++ app/calculator/Calculator.tsx
@@ -11,7 +11,9 @@ function Calculator() {
 
 	return (
 		<>
-			<div className='w-[45vw] h-[93vh] bg-black fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold text-[35px]'>
+			<div className='w-[430px] h-[860px] rounded-md bg-black fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold text-[35px]'>
+				{/* 1st ROW */}
+
 				<button
 					className='w-1/5 h-[10%] rounded-full bg-[#a5a5a5] fixed top-[35vh] left-[4%] text-black'
 					onClick={() => UpdateNum(0)}
@@ -37,6 +39,63 @@ function Calculator() {
 					onClick={() => UpdateNum(0)}
 				>
 					%
+				</button>
+				<button
+					className='w-1/5 h-[10%] rounded-full bg-[#ff9f0a] fixed top-[35vh] left-[76%] text-[#fffeff]'
+					onClick={() => UpdateNum(0)}
+				>
+					<div className='w-[5px] h-[5px] rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-3 bg-[#fffeff]'></div>
+					<div className='w-[5px] h-[5px] rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1.5 bg-[#fffeff]'></div>
+					<div className='w-[28%] h-[4px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#fffeff]'></div>
+				</button>
+
+				{/* 2nd ROW */}
+
+				<button
+					className='w-1/5 h-[10%] rounded-full bg-[#333333] fixed top-[46vh] left-[4%] text-white font-normal text-[40px]'
+					onClick={() => UpdateNum(0)}
+				>
+					7
+				</button>
+				<button
+					className='w-1/5 h-[10%] rounded-full bg-[#333333] fixed top-[46vh] left-[28%] text-white font-normal text-[40px]'
+					onClick={() => UpdateNum(0)}
+				>
+					8
+				</button>
+				<button
+					className='w-1/5 h-[10%] rounded-full bg-[#333333] fixed top-[46vh] left-[52%] text-white font-normal text-[40px]'
+					onClick={() => UpdateNum(0)}
+				>
+					9
+				</button>
+				<button
+					className='w-1/5 h-[10%] rounded-full bg-[#ff9f0a] fixed top-[46vh] left-[76%] text-[#fffeff]'
+					onClick={() => UpdateNum(0)}
+				>
+					<div className='w-[33%] h-[4px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#fffeff] rotate-45'></div>
+					<div className='w-[33%] h-[4px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#fffeff] -rotate-45'></div>
+				</button>
+
+				{/* 3rd ROW */}
+
+				<button
+					className='w-1/5 h-[10%] rounded-full bg-[#333333] fixed top-[57vh] left-[4%] text-white font-normal text-[40px]'
+					onClick={() => UpdateNum(0)}
+				>
+					4
+				</button>
+				<button
+					className='w-1/5 h-[10%] rounded-full bg-[#333333] fixed top-[57vh] left-[28%] text-white font-normal text-[40px]'
+					onClick={() => UpdateNum(0)}
+				>
+					5
+				</button>
+				<button
+					className='w-1/5 h-[10%] rounded-full bg-[#333333] fixed top-[57vh] left-[52%] text-white font-normal text-[40px]'
+					onClick={() => UpdateNum(0)}
+				>
+					6
 				</button>
 			</div>
 		</>
